0dd897be2e73b61cdba23ff7c409077e020e0e20
charu-blabla <malhotracharu56@gmail.com>
2026-08-30 18:57:09 +0530

    Correct day1_basics.py

--- day1_basics.py
+++ day1_basics.py
@@ -1,3 +1,3 @@
 name =input("enter name")
 age =int(input("enter age"))
-print(f"Hello Aaron, you are {age} years old. In 5 years, you will be {age+5}.")
+print(f"Hello {name}, you are {age} years old. In 5 years, you will be {age+5}.")
