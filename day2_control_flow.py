d633902f674bfb3944aa7fd764f850eeb9b1a6d8
charu-blabla <malhotracharu56@gmail.com>
2026-09-01 19:51:05 +0530

    Implement grading system based on marks input

---
+++ day2_control_flow.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter Marks: ")
+if marks>=90:
+            print("A")
+elif marks>=75 or marks<90:
+            print("B")
+elif marks>=50 or marks<75:
+            print("C")
+elif marks<=50:
+            print("Fail")
+else:
+            print("Invalid Input")
+
