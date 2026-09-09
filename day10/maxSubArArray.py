f5ab96d5576fd32605be312b76d78b98f8c48fc9
charu-blabla <malhotracharu56@gmail.com>
2026-09-09 22:32:25 +0530

    Implement maxSubArray method for maximum sum

---
+++ day10/maxSubArArray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        maxSum =nums[0]
+        currentSum = nums[0]
+
+        for i in nums[1: ]:
+
+            currentSum = max(i,currentSum+i)
+            maxSum = max(maxSum,currentSum)
+        return maxSum
+   
