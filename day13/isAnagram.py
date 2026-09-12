082ff008870f54fa05b40107420d57fc41edddfe
charu-blabla <malhotracharu56@gmail.com>
2026-09-12 22:39:42 +0530

    Add isAnagram function to check anagrams
    Implement a function to check if two strings are anagrams.

---
+++ day13/isAnagram.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(t) != len(s):
+            return False
+        
+        freqS = {}
+        freqT = {}
+        
+        for char in s:
+            if char in freqS:
+                freqS[char]+=1
+            else:
+                freqS[char] = 1
+        for char in t:
+            if char in freqT:
+                freqT[char]+=1
+            else:
+                freqT[char] = 1
+        
+        return freqT == freqS
+        
+        
