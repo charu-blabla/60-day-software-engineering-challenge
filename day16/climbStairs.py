c1611ff54082e5ae8ddd9f5a64198d45054392d2
charu-blabla <malhotracharu56@gmail.com>
2026-09-15 00:21:53 +0530

    Implement climbStairs function for Fibonacci sequence

---
+++ day16/climbStairs.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        if n <=2 :
+            return n
+        
+        a =1
+        b = 2
+        c  = 0
+        for i in range(3,n+1):
+            c = a+b
+            a = b
+            b = c
+
+        return c
+
+         
